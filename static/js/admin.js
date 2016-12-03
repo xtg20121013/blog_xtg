b97f7180488331267dc6c3387d4ba90e4feb7533
mhq <imgamer@qq.com>
2016-12-03 19:23:12 +0800

    修改密码 两次密码验证

--- static/js/admin.js
+++ static/js/admin.js
@@ -176,3 +176,17 @@ function changePassword() {
 function editUserInfo() {
     $('#editUserInfoFormModal').modal();
 }
+
+//js for checkDoublePassword
+function checkChangePasswordForm() {
+    var password = $('#password').val();
+    var password2 = $('#password2').val();
+    if (password != password2) {
+        $('#group_password2').addClass('has-error');
+        $('#password2_err').show();
+        return false;
+    } else {
+        $('#changePasswordForm').submit();
+        return true;
+    }
+}
