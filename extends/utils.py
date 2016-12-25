09fb79d359af56c6486c9eee9c566931b26798fc
mhq <imgamer@qq.com>
2016-12-25 21:04:51 +0800

    解决SQLalchemy 对象中存在循环嵌套（子节点中含有父节点对象）导致json dump无限递归的问题

--- extends/utils.py
+++ extends/utils.py
@@ -17,15 +17,25 @@ def singleton(cls, *args, **kw):
 
 
 class AlchemyEncoder(json.JSONEncoder):
+
+    def __init__(self, dumps_objs=None, *w, **kw):
+        super(AlchemyEncoder, self).__init__(*w, **kw)
+        if dumps_objs is None:
+            dumps_objs = []
+        self.dumps_objs = dumps_objs
+
     def default(self, o):
         if isinstance(o.__class__, DeclarativeMeta):
+            self.dumps_objs.append(o)
             data = {}
             fields = o.__json__() if hasattr(o, '__json__') else dir(o)
             fields = [f for f in fields if not f.startswith('_') and f not in ['metadata', 'query', 'query_class']]
             for field in fields:
                 value = o.__getattribute__(field)
+                if value and self.dumps_objs and value in self.dumps_objs:
+                    continue
                 try:
-                    json.dumps(value, cls=AlchemyEncoder)
+                    json.dumps(value, cls=AlchemyEncoder, dumps_objs=self.dumps_objs)
                     data[field] = value
                 except TypeError:
                     pass
