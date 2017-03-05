920420885ec8e09e1fa7cebb454e11aadf8490d5
mhq <imgamer@qq.com>
2017-03-05 12:49:22 +0800

    评论回复

--- static/js/articleDetail.js
+++ static/js/articleDetail.js
@@ -1,21 +1,17 @@
 
-//Rest the value when reply-dialog-box dismiss
-function undo_reply() {
-    $('#follow').val(-1);
-}
-
-function go_to_reply(id, author_name) {
-    $('html, body').animate({scrollTop:  $('#submit-comment').offset().top}, 800);
-    $('#follow').val(id);
+function go_to_reply(comment_type, reply_to_id, reply_to_floor) {
     $('#reply-dialog-box').remove();
-    $('#submit-comment-container').prepend('<div class="alert alert-info alert-dismissable" id="reply-dialog-box">' +
-            '<button type="button" class="close" data-dismiss="alert" onclick="undo_reply()">&times;</button>' +
-            '回复给<strong><i>' + author_name +'</i></strong> </div>');
+    $('#submit-comment-form').prepend('<div class="alert alert-info alert-dismissable" id="reply-dialog-box">' +
+            '<input type="hidden" name="comment_type", value="'+comment_type+'">'+
+            '<input type="hidden" name="reply_to_id", value="'+reply_to_id+'">'+
+            '<input type="hidden" name="reply_to_floor", value="'+reply_to_floor+'">'+
+            '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+            '回复给<strong><i>' + reply_to_floor +'</i></strong> </div>');
+    $('html, body').animate({scrollTop:$('#submit-comment').offset().top});
 }
 
 //Reset the follow value when refresh page
 window.onload = function(){
-    $('#follow').val(-1);
     var content = $('.article-content').text();
     $('.article-content').html(markdown.toHTML(content));
     $('.article-loading').hide();
